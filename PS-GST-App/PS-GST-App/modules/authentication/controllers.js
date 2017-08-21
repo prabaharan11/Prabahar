3e138c95bdf143f8d5faba7f5049d96ca22c16b6
prabaharan11 <prabaharan0101@gmail.com>
2017-08-21 22:32:20 +0530

    Add files via upload

---
+++ PS-GST-App/PS-GST-App/modules/authentication/controllers.js
@@ -0,0 +1,41 @@
+'use strict';
+
+ 
+angular.module('Authentication')
+ 
+.controller('LoginController',
+    ['$scope', '$rootScope', '$location', 'AuthenticationService',
+    function ($scope, $rootScope, $location, AuthenticationService)
+ {
+                
+AuthenticationService.ClearCredentials();
+ 
+        
+$scope.login = function () 
+{
+            
+$scope.dataLoading = true;
+            
+AuthenticationService.Login($scope.username, $scope.password, function(response) 
+{
+               
+ if(response.success) {
+                    
+AuthenticationService.SetCredentials($scope.username, $scope.password);
+                    
+$location.path('/');
+                } 
+else 
+{
+                    
+$scope.error = response.message;
+                    
+$scope.dataLoading = false;
+                
+}
+           
+ });
+       
+ };
+    
+}]);
---
+++ PS-GST-App/PS-GST-App/modules/home/controllers.js
@@ -0,0 +1,10 @@
+'use strict';
+ 
+
+angular.module('Home')
+ 
+.controller('HomeController',
+    ['$scope',
+    function ($scope) {
+      
+    }]);
